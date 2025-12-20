a0c776254af18a3ee74d6f8cf8918d3dca70e629
Byte206 <gabimagister@outlook.es>
2025-12-20 23:57:10 +0100

    test on 42 cluster needed

--- actions.c
+++ actions.c
@@ -92,5 +92,18 @@ void	philo_sleep(t_philosopher *philo)
 
 void	philo_think(t_philosopher *philo)
 {
+	long	think_time;
+
 	print_status(philo, "is thinking");
+	if (philo->table->number_of_philosophers % 2 != 0)
+	{
+		think_time = (philo->table->time_to_eat * 2)
+			- philo->table->time_to_sleep;
+		if (think_time < 0)
+			think_time = 0;
+		if (think_time > 600)
+			think_time = 200;
+		if (think_time > 0)
+			precise_usleep(think_time, philo->table);
+	}
 }
